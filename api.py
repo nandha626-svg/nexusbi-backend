4df7e490b000c96382eb0591e982821a2e27d9dd
nandha626-svg <nandha626@gmail.com>
2026-06-03 02:33:04 -0500

    Allow all CORS origins for testing

--- api.py
+++ api.py
@@ -14,10 +14,8 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"
-    "https://nexusbi-frontend-three.vercel.app"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
